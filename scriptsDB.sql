d433f18b651f0efc6881562e1a6dc96b10f6c8f1
Rodrigo Martinho Lopez <rodrigo.martinho.lopez@gmail.com>
2026-03-16 15:44:40 -0300

    Update scriptsDB.sql

--- scriptsDB.sql
+++ scriptsDB.sql
@@ -1,2 +1,28 @@
+# Instalação do MySQL no Linux
+  
+→ sudo apt install mysql-server -y
+
+→ sudo mysql_secure_installation 
+
+Selecione o método de senha 0 - Baixo | 1 - Medio | 2 - Alto e o restante coloquei tudo Y (sim)
+
+→ sudo mysql (abrir o mysql)
+
+# Criar o Database
+CREATE DATABASE IoT;
+
+# Criar usuário e senha
+CREATE USER 'iot'@'localhost' IDENTIFIED BY 'IoT2026#'; 
+
+# Dar permissão para o usuário no banco criado
+GRANT ALL PRIVILEGES ON IoT.* TO 'iot'@'localhost'; 
+
+FLUSH PRIVILEGES;
+EXIT;
+
+→ mysql -u meu_usuario -p (inserir a senha do usuário)
+
+USE IoT;
+
 CREATE TABLE RegistroLuzes (id int NOT NULL AUTO_INCREMENT, data_hora timestamp NULL DEFAULT ((now() - interval 3 hour)), status varchar(3) DEFAULT 'OFF', PRIMARY KEY (id));
 CREATE TABLE Medicoes (id int NOT NULL auto_increment, data_hora timestamp NULL DEFAULT ((now() - interval 3 hour)), sensor varchar(10), valor decimal(5,2), PRIMARY KEY (id));
